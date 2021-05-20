b60f95127d76f4687d40725832f4b36db594d950
Carlos Draper Giggs <carjimfa@gmail.com>
2021-05-20 13:23:15 +0200

    Refactor and passing height to project cover in home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,8 +12,6 @@ export class HomeComponent implements OnInit {
 
   post$: Subject<Post> = new Subject<Post>();
 
-  featuredImageSrc$ = new Subject<string>();
-
   @ViewChild('mainArticle')
   mainArticle?: ElementRef;
 
