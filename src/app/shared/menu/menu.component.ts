f0ab1c3a256a430c26982f6a7ccdd72841c336fe
Carlos Draper Giggs <carjimfa@gmail.com>
2021-07-26 14:18:47 +0200

    Fixes centering content in Menu items

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -88,7 +88,7 @@ export class MenuComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   divideInSpan(): void {
-    const textWrappers = document.querySelectorAll('.menu__sides__navigation mat-list-item a');
+    const textWrappers = document.querySelectorAll('.menu__sides__navigation mat-list-item h1 a');
     textWrappers.forEach((e) => {
       if (!!e && !!e.textContent) {
         e.innerHTML = e.textContent.replace(/\S/g, '<span class=\'letter\' style=\'display:inline-block;\'>$&</span>');
